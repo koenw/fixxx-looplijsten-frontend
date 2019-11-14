718c510345e9514c5910c437d2c5f6491a9e845f
Gilles Wittenberg <mail@gilleswittenberg.com>
2019-11-14 16:49:34 +0100

    Clean up

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -5,10 +5,11 @@ import authToken from "../utils/authToken"
 
 const navigateToLogin = () => {
   const pathname = encodeURIComponent(window.location.pathname)
-  navigate('/login')
+  navigate("/login")
+  //navigate(`/login?redirect=${ pathname }`)
 }
 
-const useFetch = (path: string, plural = false): [any, boolean, ErrorMessage] => {
+const useFetch = (path: string, plural = false) : [any, boolean, ErrorMessage] => {
 
   const [isFetching, setIsFetching] = useState(true)
   const [error, setError] = useState<ErrorMessage>()
@@ -20,33 +21,37 @@ const useFetch = (path: string, plural = false): [any, boolean, ErrorMessage] =>
 
     (async () => {
       try {
+
         const url = getUrl(path)
         const token = authToken.get()
+
         if (token === undefined) {
           navigateToLogin()
-          return
-        }
-        const response = await fetch(url, {
-          headers: {
-            "Authorization": `Token ${ token }`
-          }
-        })
-
-        if (response.status === 403) {
-          navigateToLogin()
-        }
-        else if (response.ok) {
-          const json = await response.json()
-          setData(json)
         }
         else {
-          setError(`Error: HTTP ${ response.status }`)
-        }
+          const response = await fetch(url, {
+            headers: {
+              "Authorization": `Token ${ token }`
+            }
+          })
 
-      } catch (err) {
+          if (response.status === 403) {
+            navigateToLogin()
+          }
+          else if (response.ok) {
+            const json = await response.json()
+            setData(json)
+          }
+          else {
+            setError(`Error: HTTP ${ response.status }`)
+          }
+        }
+      }
+      catch (err) {
         console.error(err)
         setError(err)
-      } finally {
+      }
+      finally {
         setIsFetching(false)
       }
     })()
