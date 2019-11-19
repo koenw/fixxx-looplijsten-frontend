80f5611029eb7b01dfc019b9a6d6c9c74caa97d8
Gilles Wittenberg <mail@gilleswittenberg.com>
2019-11-19 15:22:34 +0100

    Navigation styling

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,14 +1,39 @@
 import React, { FC } from "react"
 import { Link } from "@reach/router"
+import styled from "styled-components"
+import { Search as SearchIcon } from "@datapunt/asc-assets"
+
+const Nav = styled.nav`
+  background-color: #E6E6E6
+  margin: -12px
+  margin-bottom: 12px
+  padding: 12px
+`
+const Ul = styled.ul`
+  list-style: none
+  margin: 0
+  padding: 0
+  display: flex
+  justify-content: space-between
+`
+const Li = styled.li`
+  a {
+    color: black
+  }
+`
+const LiSearch = styled.li`
+  width: 24px
+  height: 24px
+`
 
 const Navigation: FC = () => {
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">Mijn looplijst</Link></li>
-        <li><Link to="/zoeken">Zoeken</Link></li>
-      </ul>
-    </nav>
+    <Nav>
+      <Ul>
+        <Li><Link to="/">Mijn looplijst</Link></Li>
+        <LiSearch><Link to="/zoeken"><SearchIcon /></Link></LiSearch>
+      </Ul>
+    </Nav>
   )
 }
 export default Navigation
