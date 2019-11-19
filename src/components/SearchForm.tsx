67b8c5d3fce451405c08d683f9c42e46521b9504
Gilles Wittenberg <mail@gilleswittenberg.com>
2019-11-19 14:44:05 +0100

    SearchForm styling

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,13 +1,32 @@
 import React, { FC, FormEvent, Dispatch, SetStateAction } from "react"
 import styled from "styled-components"
 import { Button } from "@datapunt/asc-ui"
+import { Search } from "@datapunt/asc-assets"
 import useOnChangeState from "../hooks/useOnChangeState"
 import { getUrl } from "../config/domain"
-import Input from "./Input"
+import InputBase from "./Input"
 
 type Props = {
-  setResults: Dispatch<SetStateAction<never[]>>
+  setResults: SetState
 }
+
+const Form = styled.form`
+  max-width: 768px
+`
+
+const Label = styled.label`
+  font-weight: 500
+  display: block
+  margin-bottom: 2px
+`
+
+const InputWrap = styled.div`
+  display: inline-block
+`
+const Input = styled(InputBase)`
+  height: 44px
+  width: calc(100% - 4px)
+`
 
 const InputSearchForm = styled(Input)`
   height: 44px
@@ -15,21 +34,31 @@ const InputSearchForm = styled(Input)`
   vertical-align: top
 `
 
-const InputPostalCode = styled(InputSearchForm)`
-  width: 120px
+const InputWrapPostalCode = styled(InputWrap)`
+  width: calc(100% - 198px)
 `
 
-const InputNumber = styled(InputSearchForm)`
-  width: 40px
-  &::-webkit-inner-spin-button,
-  &::-webkit-outer-spin-button {
-    -webkit-appearance: none
-    margin: 0
+const InputWrapStreetNumber = styled(InputWrap)`
+  width: 72px
+  input {
+    &::-webkit-inner-spin-button,
+    &::-webkit-outer-spin-button {
+      -webkit-appearance: none
+      margin: 0
+    }
   }
 `
 
-const InputAddition = styled(InputSearchForm)`
-  width: 40px
+const InputWrapSuffix = styled(InputWrap)`
+  width: 72px
+`
+
+const ButtonWrap = styled.div`
+  vertical-align: top
+  display: inline-block
+`
+const SearchButton = styled(Button)`
+  margin-top: 24px
 `
 
 const SearchForm: FC<Props> = ({ setResults }) => {
@@ -57,35 +86,43 @@ const SearchForm: FC<Props> = ({ setResults }) => {
   }
 
   return (
-    <form onSubmit={ onSubmit }>
-      <InputPostalCode
-        type="text"
-        placeholder="postcode"
-        pattern="[1-9][0-9]{3}\s?[a-zA-Z]{2}"
-        title="Geldige postcodes zijn 1234AA of 1234 aa"
-        required
-        value={ postalCode }
-        onChange={ onChangePostalCode }
-        />
-      <InputNumber
-        type="number"
-        placeholder="huisnummer"
-        min="1"
-        step="1"
-        pattern="\d+"
-        title="Alleen cijfers zijn geldig"
-        required
-        value={ streetNumber }
-        onChange={ onChangeStreetNumber }
-        />
-      <InputAddition
-        type="text"
-        placeholder="toevoeging"
-        value={ suffix }
-        onChange={ onChangeSuffix }
-        />
-      <Button variant="primary">Zoek</Button>
-    </form>
+    <Form onSubmit={ onSubmit }>
+      <InputWrapPostalCode>
+        <Label>postcode</Label>
+        <Input
+          type="text"
+          pattern="[1-9][0-9]{3}\s?[a-zA-Z]{2}"
+          title="Geldige postcodes zijn 1234AA of 1234 aa"
+          required
+          value={ postalCode }
+          onChange={ onChangePostalCode }
+          />
+      </InputWrapPostalCode>
+      <InputWrapStreetNumber>
+        <Label>huisnr.</Label>
+        <Input
+          type="number"
+          min="1"
+          step="1"
+          pattern="\d+"
+          title="Alleen cijfers zijn geldig"
+          required
+          value={ streetNumber }
+          onChange={ onChangeStreetNumber }
+          />
+      </InputWrapStreetNumber>
+      <InputWrapSuffix>
+        <Label>toev.</Label>
+        <Input
+          type="text"
+          value={ suffix }
+          onChange={ onChangeSuffix }
+          />
+      </InputWrapSuffix>
+      <ButtonWrap>
+        <SearchButton variant="secondary" iconSize={ 24 } icon={ <Search /> } />
+      </ButtonWrap>
+    </Form>
   )
 }
 
