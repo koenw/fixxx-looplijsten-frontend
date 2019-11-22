0b86d1277b95317de4123d396d5b02d61cb06bb5
Peter Curet <>
2019-11-22 22:24:46 +0100

    Cleared search form

--- src/components/search/SearchForm.tsx
+++ src/components/search/SearchForm.tsx
@@ -54,8 +54,8 @@ const SearchButton = styled(Button)`
 
 const SearchForm: FC<Props> = ({ setResults }) => {
 
-  const [postalCode, onChangePostalCode] = useOnChangeState("1234AA")
-  const [streetNumber, onChangeStreetNumber] = useOnChangeState("1")
+  const [postalCode, onChangePostalCode] = useOnChangeState("")
+  const [streetNumber, onChangeStreetNumber] = useOnChangeState("")
   const [suffix, onChangeSuffix] = useOnChangeState()
   const onSubmit = async (event: FormEvent) => {
     event.preventDefault()
