2b07c6ad32ab811b7dd0608142d17807f1d3ee6f
Gilles Wittenberg <mail@gilleswittenberg.com>
2019-12-05 14:41:11 +0100

    Fix iPhone input styling

--- src/components/styled/Input.tsx
+++ src/components/styled/Input.tsx
@@ -3,9 +3,11 @@ import styled from "styled-components"
 export default styled.input`
   box-sizing: border-box
   border: 1px solid rgb(118, 118, 118);
+  border-radius: 0
   font-size: 1rem
   line-height: 18px
   padding: 10px
+  -webkit-appearance: none
   &:focus {
     outline: rgb(254, 200, 19) solid 2px
     outline-offset: 0.5px
