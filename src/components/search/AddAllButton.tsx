7fd62235f58f807df2b318b3395aee94eeb2be0a
Gilles Wittenberg <mail@gilleswittenberg.com>
2019-12-12 12:10:13 +0100

    Quick fix disabling AddAllButton

--- src/components/search/AddAllButton.tsx
+++ src/components/search/AddAllButton.tsx
@@ -1,11 +1,31 @@
-import React, { FC, MouseEvent } from "react"
+import React, { FC, useState, MouseEvent } from "react"
 import { Button } from "@datapunt/asc-ui"
 import { Enlarge } from "@datapunt/asc-assets"
+import noop from "../../utils/noop"
 
 type Props = {
   onClick?: (a: MouseEvent<HTMLButtonElement>) => void
 }
 
-const AddAllButton: FC<Props> = ({ onClick }) =>
-  <Button onClick={ onClick } variant="blank" iconLeft={ <Enlarge /> }>Voeg alles toe aan Mijn looplijst</Button>
+const AddAllButton: FC<Props> = ({ onClick = noop }) => {
+
+  const [isDisabled, setIsDisabled] = useState(false)
+  const disable = () => setIsDisabled(true)
+
+  const onClickHandler = (event: MouseEvent<HTMLButtonElement>) => {
+    disable()
+    onClick(event)
+  }
+
+  return (
+    <Button
+      onClick={ onClickHandler }
+      variant="blank"
+      iconLeft={ <Enlarge /> }
+      disabled={ isDisabled }
+    >
+    Voeg alles toe aan Mijn looplijst
+    </Button>
+  )
+}
 export default AddAllButton
