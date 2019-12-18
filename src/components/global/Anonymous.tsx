90916f4d5d1e2016b381efbd4ea62d12866fca0a
Gilles Wittenberg <mail@gilleswittenberg.com>
2019-12-18 10:49:18 +0100

    Fix anonymous anchor blurring

--- src/components/global/Anonymous.tsx
+++ src/components/global/Anonymous.tsx
@@ -3,6 +3,6 @@ import StateContext from "../../contexts/StateContext"
 
 const Anonymous: FC = () => {
   const { state: { isAnonymous } } = useContext(StateContext)
-  return isAnonymous ? <style>{ ".anonymous { color: transparent; text-shadow: 0 0 15px rgba(0,0,0,0.5); }" }</style> : null
+  return isAnonymous ? <style>{ ".anonymous, .anonymous a { color: transparent; text-shadow: 0 0 15px rgba(0,0,0,0.5); }" }</style> : null
 }
 export default Anonymous
