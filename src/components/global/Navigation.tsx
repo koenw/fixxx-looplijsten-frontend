e7d4e129ee59196a4ac971797758bec0f04d5488
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-01-08 17:05:03 +0100

    Page indicators

--- src/components/global/Navigation.tsx
+++ src/components/global/Navigation.tsx
@@ -14,8 +14,9 @@ const NavWrap = styled.div`
 
 const Nav = styled.nav`
   background-color: #E6E6E6
-  margin-bottom: 12px
   padding: 12px
+  padding-bottom: 0
+  margin-bottom: 12px
   width: calc(100% - 24px);
 `
 const Ul = styled.ul`
@@ -26,6 +27,9 @@ const Ul = styled.ul`
   justify-content: space-between
 `
 const Li = styled.li`
+  border-bottom: 5px solid transparent
+  border-bottom-color: ${ (props: { isActive?: boolean }) => props.isActive ? 'red' : 'transparent' }
+  padding-bottom: 7px
   a {
     color: black
   }
@@ -34,6 +38,9 @@ const LiSearch = styled.li`
   width: 24px
   height: 24px
   margin-right: 16px
+  border-bottom: 5px solid transparent
+  border-bottom-color: ${ (props: { isActive?: boolean }) => props.isActive ? 'red' : 'transparent' }
+  padding-bottom: 7px
 `
 
 const Navigation: FC = () => {
@@ -49,12 +56,16 @@ const Navigation: FC = () => {
   const numItineraries = itineraries ? itineraries.length : 0
   const showCounter = numItineraries > 0
 
+  const pathname = window.location.pathname
+  const looplijstActive = pathname === to("/", false)
+  const searchActive = pathname === to("/zoeken", false) || pathname === to("/parse", false)
+
   return (
     <NavWrap>
       <Nav>
         <Ul>
-          <Li><Link to={ to("/") }>Mijn looplijst { showCounter && `(${ numItineraries })` }</Link></Li>
-          <LiSearch><Link to={ to("/zoeken") }><SearchIcon /></Link></LiSearch>
+          <Li isActive={ looplijstActive }><Link to={ to("/") }>Mijn looplijst { showCounter && `(${ numItineraries })` }</Link></Li>
+          <LiSearch isActive={ searchActive }><Link to={ to("/zoeken") }><SearchIcon /></Link></LiSearch>
         </Ul>
       </Nav>
     </NavWrap>
