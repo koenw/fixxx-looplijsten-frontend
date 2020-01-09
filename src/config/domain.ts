2048909fa3890f1bf422ba3daaa887367b82be2f
Peter Curet <>
2020-01-09 15:01:29 -0400

    Renamed auth path

--- src/config/domain.ts
+++ src/config/domain.ts
@@ -12,7 +12,7 @@ const domain =
   isAcc ? "https://acc.api.straatnotes.amsterdam.nl/" :
   "http://localhost:8000/"
 const basePath = "api/v1/"
-const authPath = "token_obtain_pair/"
+const authPath = "credentials-authenticate/"
 const authOIDCPath = "oidc-authenticate/"
 const pathPrefix = "looplijsten/"
 
