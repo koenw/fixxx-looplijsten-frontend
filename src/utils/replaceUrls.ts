933397e4f59a7168052a992b38c56f6eabc91f7c
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-01-09 10:04:50 +0100

    Improve replaceUrl

--- src/utils/replaceUrls.ts
+++ src/utils/replaceUrls.ts
@@ -1,6 +1,10 @@
 // @LINK: https://stackoverflow.com/questions/3809401/what-is-a-good-regular-expression-to-match-a-url
 // eslint-disable-next-line no-useless-escape
 const regExp = /((https?:\/\/)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*))/g
-const replaceWith = "<a href='$1'>$1</a>"
+const replaceWith = (g0: string, _: string, g2: string | undefined) => {
+  const text = g0
+  const url = `${ g2 === undefined ? "http://" : "" }${ text }`
+  return `<a href="${ url }">${ text }</a>`
+}
 const replaceUrls = (text: string) => text.replace(regExp, replaceWith)
 export default replaceUrls
