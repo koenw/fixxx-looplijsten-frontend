f1db98aeff052c1a736089225bf9a9c42dcae600
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-01-13 09:32:53 +0100

    Fix one length domains

--- src/utils/replaceUrls.test.ts
+++ src/utils/replaceUrls.test.ts
@@ -7,15 +7,20 @@ it("no match", () => {
 
 it("http", () => {
   const str = "http://www.example.com"
-  expect(replaceUrls(str)).toBe("<a href='http://www.example.com'>http://www.example.com</a>")
+  expect(replaceUrls(str)).toBe('<a href="http://www.example.com">http://www.example.com</a>')
 })
 
 it("in text", () => {
   const str = "Word http://www.example.com\nNext"
-  expect(replaceUrls(str)).toBe("Word <a href='http://www.example.com'>http://www.example.com</a>\nNext")
+  expect(replaceUrls(str)).toBe('Word <a href="http://www.example.com">http://www.example.com</a>\nNext')
 })
 
 it("multiple", () => {
   const str = "http://www.example.com\nhttps://www.example.com"
-  expect(replaceUrls(str)).toBe("<a href='http://www.example.com'>http://www.example.com</a>\n<a href='https://www.example.com'>https://www.example.com</a>")
+  expect(replaceUrls(str)).toBe('<a href="http://www.example.com">http://www.example.com</a>\n<a href="https://www.example.com">https://www.example.com</a>')
+})
+
+it("one char domains", () => {
+  const str = "n.v.t."
+  expect(replaceUrls(str)).toBe("n.v.t.")
 })
--- src/utils/replaceUrls.ts
+++ src/utils/replaceUrls.ts
@@ -1,6 +1,6 @@
 // @LINK: https://stackoverflow.com/questions/3809401/what-is-a-good-regular-expression-to-match-a-url
 // eslint-disable-next-line no-useless-escape
-const regExp = /((https?:\/\/)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*))/g
+const regExp = /((https?:\/\/)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-zA-Z0-9()]{2,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*))/g
 const replaceWith = (g0: string, _: string, g2: string | undefined) => {
   const text = g0
   const url = `${ g2 === undefined ? "http://" : "" }${ text }`
