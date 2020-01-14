9236e2ae1e87d5d23915342dd615d8376ddf1b69
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-01-14 10:18:01 +0100

    Fix authToken tests

--- src/lib/authToken.test.ts
+++ src/lib/authToken.test.ts
@@ -1,5 +1,7 @@
 import authToken from "./authToken"
 
+const token = "eyJ0eXAhOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl10eXBlIjoiYWNjZXNzIiwiZXhwIjoxNTc5MDA1NTQ2LCJqdGkiOiJjZTg0OWRhNTU5OTA0NDQ1OGI4MmQzOGJkZTY5ZTkyYSIsInVzZXJfaWQiOiJiNDIwNDZhMi1kMTYyLTRiMDQtYTI4Yi0zZGQ3OTFkYWE2ODgifQ.7Maem5BIBFI1mkN1Q2tMtb7m984UhlxaOXYrAofdQII"
+
 describe("authToken", () => {
 
   it("get", () => {
@@ -8,17 +10,17 @@ describe("authToken", () => {
 
   it("set", () => {
     expect(authToken.set()).toBe(false)
-    expect(authToken.set("0123456789abcdef1234567890abcdef01234567")).toBe(true)
+    expect(authToken.set(token)).toBe(true)
   })
 
   it("set, get", () => {
-    authToken.set("0123456789abcdef1234567890abcdef01234567")
-    expect(authToken.get()).toBe("0123456789abcdef1234567890abcdef01234567")
+    authToken.set(token)
+    expect(authToken.get()).toBe(token)
   })
 
   it("clear", () => {
     expect(authToken.clear()).toBe(true)
-    authToken.set("0123456789abcdef1234567890abcdef01234567")
+    authToken.set(token)
     authToken.clear()
     expect(authToken.get()).toBeUndefined()
   })
