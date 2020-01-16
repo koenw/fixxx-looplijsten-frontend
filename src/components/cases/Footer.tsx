99721d351d3fd2de08418c8e6c0d85516e69ae5b
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-01-16 16:26:21 +0100

    Styling

--- src/components/cases/Footer.tsx
+++ src/components/cases/Footer.tsx
@@ -2,8 +2,8 @@ import styled from "styled-components"
 
 const Footer = styled.footer`
   border-top: 1px solid #B4B4B4
-  margin: 0 -8px
-  padding: 12px 0 4px
+  margin: 0 -15px
+  padding-top: 15px
   text-align: center
   a {
     margin-top: 8px
