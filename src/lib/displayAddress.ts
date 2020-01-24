e3b5fb13a02b178340dafc598487b90e848cbaec
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-01-24 11:13:22 +0100

    Display address according to BAG

--- src/lib/displayAddress.ts
+++ src/lib/displayAddress.ts
@@ -1,11 +1,4 @@
-const displayAddress = (streetName: string, streetNumber: string | number, suffix?: string, etage?: string | number) => {
-  // @TODO: Normalize etage and suffix
-  /*
-  const showHyphen = etage !== undefined && suffix === undefined
-  const showSpace = !showHyphen && (suffix !== undefined || etage !== undefined)
-  const connection = showHyphen ? "-" : showSpace ? " " : ""
-  */
-  const connection = suffix !== undefined || etage !== undefined ? " " : ""
-  return `${ streetName } ${ streetNumber }${ connection }${ suffix ? suffix : "" }${ etage ? etage : "" }`
-}
+const displayAddress = (streetName: string, streetNumber: string | number, suffix?: string, etage?: string | number) =>
+  `${ streetName } ${ streetNumber }${ suffix ? suffix : "" }${ etage ? `-${ etage }` : "" }`
+  
 export default displayAddress
