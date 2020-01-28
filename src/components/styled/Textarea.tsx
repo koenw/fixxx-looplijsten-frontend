7fb82d6274afa19e014c9e7159643bb3030bdee2
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-01-28 16:05:52 +0100

    Textarea webkit appearance

--- src/components/styled/Textarea.tsx
+++ src/components/styled/Textarea.tsx
@@ -6,6 +6,7 @@ export default styled.textarea`
   font-size: 1rem
   line-height: 18px
   padding: 10px
+  -webkit-appearance: none
   &:focus {
     outline: rgb(254, 200, 19) solid 2px
     outline-offset: 0.5px
