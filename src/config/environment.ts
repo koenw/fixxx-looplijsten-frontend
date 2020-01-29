f86ab700e2f106814732f2f062e8495fefda3ac5
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-01-29 13:57:58 +0100

    Add favicon files

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -3,5 +3,6 @@ import parseLocationSearch from "../lib/utils/parseLocationSearch"
 const hostname = window.location.hostname
 export const isProduction = hostname === "top.amsterdam.nl"
 export const isAcc = hostname === "acc.straatnotes.amsterdam.nl"
+export const isDevelopment = hostname === "localhost"
 const { api } = parseLocationSearch(window.location.search)
-export const forceAcc = api === "acc"
+export const forceAcc = isDevelopment || api === "acc"
