a4e42ba3b982c20f1ea71c19eb6c144ac830c546
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-01-29 11:36:21 +0100

    Rollback previous commit

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,20 +1,73 @@
-import React, { FC } from "react"
+import React, { FC, FormEvent } from "react"
+import useGlobalState from "../../hooks/useGlobalState"
+import useOnChangeState from "../../hooks/useOnChangeState"
 import styled from "styled-components"
+import { Button } from "@datapunt/asc-ui"
+import { Login as LoginIcon } from "@datapunt/asc-assets"
 import { getOIDCProviderUrl } from "../../config/api"
 
+import ErrorMessage from "../global/ErrorMessage"
+import Input from "../styled/Input"
+
+const Form = styled.form`
+  width: 100%
+  max-width: 300px
+`
+
 const Div = styled.div`
-  margin-top: 100px
+  margin: 32px 0 0 0
   display: flex
+  align-items: center
   justify-content: center
+  flex-direction: column
+  height: 75vh
+`
+
+const InputLoginForm = styled(Input)`
+  width: 100%
+  display: block
+  margin-bottom: 18px
 `
 
 const LoginForm: FC = () => {
 
-  const href = getOIDCProviderUrl()
+  const {
+    authenticate,
+    auth: {
+      isFetching,
+      errorMessage
+    }
+  } = useGlobalState()
+
+  const [email, onChangeEmail] = useOnChangeState()
+  const [password, onChangePassword] = useOnChangeState()
+
+  const onSubmit = async (event: FormEvent) => {
+    event.preventDefault()
+    await authenticate(email, password)
+  }
+
+  const gripUri = getOIDCProviderUrl()
+
+  const isDisabled = isFetching
+  const showErrorMessage = errorMessage !== undefined
 
   return (
-    <Div className="LoginForm">
-      <a href={ href }>Login met KPN Grip</a>
+    <Div className="Login">
+      <Form onSubmit={ onSubmit }>
+        <h1>Looplijsten vakantieverhuur login</h1>
+        <InputLoginForm type="email" placeholder="email" value={ email } onChange={ onChangeEmail } />
+        <InputLoginForm type="password" placeholder="wachtwoord" value={ password } onChange={ onChangePassword } />
+        { showErrorMessage &&
+          <ErrorMessage text={ errorMessage! } />
+        }
+        <Button variant="application" iconLeft={ <LoginIcon /> } disabled={ isDisabled }>Inloggen</Button>
+      </Form>
+      <h2>
+          <a href={gripUri}>
+            Login met KPN Grip
+          </a>
+        </h2>
     </Div>
   )
 }
