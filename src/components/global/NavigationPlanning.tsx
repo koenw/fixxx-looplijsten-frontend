25e99bd548e077d4ea8a285f25995aa6c8273c53
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-02-10 12:07:43 +0100

    Navigation planning

---
+++ src/components/global/NavigationPlanning.tsx
@@ -0,0 +1,70 @@
+import React, { FC } from "react"
+import { Link } from "@reach/router"
+import styled from "styled-components"
+import { Search as SearchIcon } from "@datapunt/asc-assets"
+import { to } from "../../config/page"
+import { isPage, isHomePage } from "../../config/page"
+import useGlobalState from "../../hooks/useGlobalState"
+
+const NavWrap = styled.div`
+  position: fixed
+  width: 100%
+  top: 50px
+  left: 0
+  z-index: 9999
+`
+
+const Nav = styled.nav`
+  background-color: #E6E6E6
+  padding: 15px
+  padding-bottom: 0
+  margin-bottom: 15px
+  width: calc(100% - 30px);
+`
+const Ul = styled.ul`
+  list-style: none
+  margin: 0
+  padding: 0
+  display: flex
+`
+const Li = styled.li`
+  border-bottom: 5px solid transparent
+  border-bottom-color: ${ (props: { isActive?: boolean }) => props.isActive ? 'red' : 'transparent' }
+  padding: 0 10px 7px 10px
+  margin-right: 24px;
+  a {
+    color: black
+  }
+`
+
+// this empty element is used to correct scroll position under fixed header, navigation
+const FocusSpacer = styled.div`
+  height: 116px
+`
+
+const NavigationPlanning: FC = () => {
+
+  const {
+    itineraries: {
+      itineraries
+    }
+  } = useGlobalState()
+
+  const generateActive = isPage("planning")
+  const resultActive = isPage("planning/result")
+
+  return (
+    <>
+      <NavWrap>
+        <Nav>
+          <Ul>
+            <Li isActive={ generateActive }><Link to={ to("planning") }>Genereer looplijsten</Link></Li>
+            <Li isActive={ resultActive }><Link to={ to("planning/result") }>Looplijsten</Link></Li>
+          </Ul>
+        </Nav>
+      </NavWrap>
+      <FocusSpacer />
+    </>
+  )
+}
+export default NavigationPlanning
--- src/pages/ParsePage.tsx
+++ src/pages/ParsePage.tsx
@@ -1,15 +1,25 @@
 import React, { FC } from "react"
 import { RouteComponentProps } from "@reach/router"
+<<<<<<< HEAD:src/pages/ParsePage.tsx
 import Navigation from "../components/global/Navigation"
 import ParseForm from "../components/search/ParseForm"
+=======
+import NavigationPlanning from "../components/global/NavigationPlanning"
+import PlanningResult from "../components/planning/PlanningResult"
+>>>>>>> Navigation planning:src/pages/PlanningResultPage.tsx
 
 type Props = RouteComponentProps
 
 const ParsePage: FC<Props> = () => {
   return (
     <>
+<<<<<<< HEAD:src/pages/ParsePage.tsx
       <Navigation />
       <ParseForm />
+=======
+      <NavigationPlanning />
+      <PlanningResult />
+>>>>>>> Navigation planning:src/pages/PlanningResultPage.tsx
     </>
   )
 }
