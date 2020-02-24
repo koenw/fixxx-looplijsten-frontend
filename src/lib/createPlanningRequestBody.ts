36e0e2d84817844a8e525cc48e4e787cd2dc3141
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-02-24 16:56:42 +0100

    Remove unused dependencies

--- src/lib/createPlanningRequestBody.ts
+++ src/lib/createPlanningRequestBody.ts
@@ -1,8 +1,6 @@
 import { openingDate, openingReasons, listsWeek, listsDay } from "../config/planning"
 
 const createPlanningRequestBody = (inputs: number[], dayOfWeek?: number) => {
-  const listLength = 6
-  const listLengthLong = 4
   const singleDay = dayOfWeek !== undefined
   const lists = singleDay ? listsDay(dayOfWeek!) : listsWeek
   inputs.forEach((input, index) => lists[index].number_of_lists = input)
